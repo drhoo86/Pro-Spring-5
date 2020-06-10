95687ae4d985c425b092acf7c6cbd69216b9615e
Dmitry Khudorozhkov <drhoo777@gmail.com>
2020-06-10 20:24:12 +0500

    Chapter 6 - RowMapper<T> example

--- src/com/apress/prospring5/ch6/JdbcContactDao.java
+++ src/com/apress/prospring5/ch6/JdbcContactDao.java
@@ -3,9 +3,12 @@ package com.apress.prospring5.ch6;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,7 +31,8 @@ public class JdbcContactDao implements ContactDao, InitializingBean {
 
     @Override
     public List<Contact> findAll() {
-        return null;
+        String sql = "select id, first_name, last_name, birth_date from contact";
+        return namedParameterJdbcTemplate.query(sql, new ContactMapper());
     }
 
     @Override
@@ -71,6 +75,18 @@ public class JdbcContactDao implements ContactDao, InitializingBean {
         }
         if (namedParameterJdbcTemplate == null) {
             throw new BeanCreationException("Null NamedParameterJdbcTemplate on ContactDao");
+        }
+    }
+
+    private static final class ContactMapper implements RowMapper<Contact> {
+        @Override
+        public Contact mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Contact contact = new Contact();
+            contact.setId(rs.getLong("id"));
+            contact.setFirstName(rs.getString("first_name"));
+            contact.setLastName(rs.getString("last_name"));
+            contact.setBirthDate(rs.getDate("birth_date"));
+            return contact;
         }
     }
 }
--- src/com/apress/prospring5/ch6/JdbcContactDaoSample.java
+++ src/com/apress/prospring5/ch6/JdbcContactDaoSample.java
@@ -2,6 +2,8 @@ package com.apress.prospring5.ch6;
 
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.util.List;
+
 public class JdbcContactDaoSample {
     public static void main(String[] args) {
         GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
@@ -9,7 +11,15 @@ public class JdbcContactDaoSample {
         ctx.refresh();
 
         ContactDao contactDao = ctx.getBean("contactDao", ContactDao.class);
-        System.out.println("Last name for contact id 1 is: " +
-                contactDao.findLastNameById(1L));
+        List<Contact> contacts = contactDao.findAll();
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+            if (contact.getContactTelDetails() != null) {
+                for (ContactTelDetail contactTelDetail : contact.getContactTelDetails()) {
+                    System.out.println("---" + contactTelDetail);
+                }
+            }
+            System.out.println();
+        }
     }
 }
