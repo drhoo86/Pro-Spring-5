69d920ab868069a240e97dce764547a784d20120
Dmitry Khudorozhkov <drhoo777@gmail.com>
2020-06-24 01:52:18 +0500

    Chapter 8 - JPA Auditing Example

---
+++ src/com/apress/prospring5/ch8/AuditorAwareBean.java
@@ -0,0 +1,12 @@
+package com.apress.prospring5.ch8;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareBean implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("prospring5");
+    }
+}
